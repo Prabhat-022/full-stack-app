894f16d2e0686ce253d39b360f203549a5ee51e9
Prabhat-022 <lograjanand1250@gmail.com>
2025-04-01 20:11:12 +0530

    update code

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-screen bg-black text-white flex items-center justify-center text-3xl'>
+      app
+    </div>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
